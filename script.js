6a4d12ab0b344371254cb075558c509a8a997309
Kerry <z1454142986@gmail.com>
2021-01-07 22:30:43 -0800

    multilingual website

--- script.js
+++ script.js
@@ -13,10 +13,12 @@ var data = {
         "soccer": "I love Real Madrid! I am a 100% MADRIDISTA! Hala Madrid!",
         "tech_skills": "Technical skills",
         "download_resume": "Download Resume",
-        "social_media": "Find me on LinkedIn & Github",
+        "social_media": "Find me on LinkedIn & GitHub",
         "project_title": "Projects",
         "bss": "A small Domain Specific Language(DSL) compiler that compiles BSS into CSS so that it allows front-end developers to write CSS with nested style rules, variables, functions, control flow, mathematical operations & inheritance.",
-        "bloggy": "A Single Page React Application which allows users to share blogs, and allows staff to filter articles by tags, monitor comments and visualize users’ statistics."
+        "bloggy": "A Single Page React Application which allows users to share blogs, and allows staff to filter articles by tags, monitor comments and visualize users’ statistics.",
+        "messageList": "A MERN (MongoDB + Express + React + Node.js) app which also uses Redux to monitor states and deploy to Heroku. MessageList allows user to add/edit/delete messages.",
+        "read_more": "Read More",
     },
     "cn": {
         "name": "周洪凯悦",
@@ -33,7 +35,9 @@ var data = {
         "social_media": "社交媒体",
         "project_title": "开源项目",
         "bss": "小型领域特定语言（DSL）编译器，它将BSS编译为CSS，以便前端开发人员可以使用嵌套的样式规则，变量，函数，控制流，数学运算和继承来编写CSS。",
-        "bloggy": "单页React应用程序，使用户可以共享博客，并允许后台按标签过滤文章，监视评论并可视化用户的统计信息。"
+        "bloggy": "单页React应用程序，使用户可以共享博客，并允许后台按标签过滤文章，监视评论并可视化用户的统计信息。",
+        "messageList": "一个MERN（MongoDB + Express + React + Node.js）应用程序，它也使用Redux监视状态并部署到Heroku。 MessageList允许用户添加/编辑/删除消息。",
+        "read_more": "戳这里",
 
     },
     "hk": {
@@ -51,7 +55,9 @@ var data = {
         "social_media": "社交媒體",
         "project_title": "開源項目",
         "bss": "小型領域特定語言（DSL）編譯器，它將BSS編譯為CSS，以便前端開發人員可以使用嵌套的樣式規則，變量，函數，控制流，數學運算和繼承來編寫CSS。",
-        "bloggy": "單頁React應用程序，使用戶可以共享博客，並允許後台按標籤過濾文章，監視評論並可視化用戶的統計信息。"
+        "bloggy": "單頁React應用程序，使用戶可以共享博客，並允許後台按標籤過濾文章，監視評論並可視化用戶的統計信息。",
+        "messageList": "一個MERN（MongoDB + Express + React + Node.js）應用程序，它也使用Redux監視狀態並部署到Heroku。 MessageList允許用戶添加/編輯/刪除消息。",
+        "read_more": "了解更多",
     },
     "jp": {
         "name": "川端春樹 かわばたはるき",
@@ -68,28 +74,88 @@ var data = {
         "social_media": "ソーシャルメディア",
         "project_title": "オープンソースプロジェクト",
         "bss": "BSSをCSSにコンパイルする小さなドメイン固有言語（DSL）コンパイラ。これにより、フロントエンド開発者はネストされたスタイルのルール、変数、関数、制御フロー、数学演算、継承を使用してCSSを記述できます。",
-        "bloggy": "単一ページのReactアプリケーションを使用すると、ユーザーはブログを共有でき、背景で記事をタグでフィルタリングしたり、コメントを監視したり、ユーザー統計を視覚化したりできます。"
+        "bloggy": "単一ページのReactアプリケーションを使用すると、ユーザーはブログを共有でき、背景で記事をタグでフィルタリングしたり、コメントを監視したり、ユーザー統計を視覚化したりできます。",
+        "messageList": "MERN（MongoDB + Express + React + Node.js）アプリケーション。これも、Reduxを使用してステータスを監視し、Herokuにデプロイします。 MessageListを使用すると、ユーザーはメッセージを追加/編集/削除できます。",
+        "read_more": "詳しくは"
     },
     "kr": {
-        "name": "주정혁" //소프트웨어 공학자
+        "name": "주정혁", //소프트웨어 공학자
+        "contact": "연락처",
+        "bgc": "테마 색상 설정",
+        "quote": "사람들이 당신에 대해 생각하는 것과 당신에 대해 어떻게 생각하는지 통제 할 수 없습니다. 불운에 대비할 수 없습니다. 최선을 다하고 정직해야합니다. 마지막으로 가장 중요한 것은 열심히 일하는 것입니다. 나머지는 통제 할 수 없습니다. ― 마리아 샤라포바, “멈출 수없는 : 오늘의 내 삶”",
+        "description_title": "개인 정보",
+        "description": "저는 풀 스택 소프트웨어 엔지니어입니다.",
+        "about_me_title": "나에 대해서",
+        "self_introduction": "브리티시 컬럼비아 대학교의 컴퓨터 학부생. 자기 관리, 완벽 추구, 강한 스트레스 내성 및 기대 이상의 열정에 능합니다.",
+        "soccer": "저는 레알 마드리드 팬입니다! 영원히 Madridist!",
+        "tech_skills": "전문 기술",
+        "download_resume": "이력서",
+        "social_media": "소셜 미디어",
+        "project_title": "오픈 소스 프로젝트",
+        "bss": "프런트 엔드 개발자가 중첩 스타일 규칙, 변수, 함수, 제어 흐름, 수학적 연산 및 상속을 사용하여 CSS를 작성할 수 있도록 BSS를 CSS로 컴파일하는 DSL (Small Domain Specific Language) 컴파일러입니다.",
+        "bloggy": "단일 페이지 React 애플리케이션을 사용하면 사용자가 블로그를 공유 할 수 있으며 배경에서 태그별로 기사를 필터링하고 댓글을 모니터링하며 사용자 통계를 시각화 할 수 있습니다.",
+        "messageList": "Redux를 사용하여 상태를 모니터링하고 Heroku에 배포하는 MERN (MongoDB + Express + React + Node.js) 애플리케이션입니다. MessageList는 사용자가 메시지를 추가 / 편집 / 삭제할 수 있도록합니다.",
+        "read_more": "추가 정보"
     },
     "es": {
-        "name": "Gabriel Zhou"
+        "name": "Gabriel Zhou",
+        "contact": "Detalles de contacto",
+        "bgc": "Establecer el color del tema",
+        "quote": "No puedes controlar lo que la gente dice de ti y lo que piensan de ti. No puedes planear para la mala suerte. Solo puedes esforzarte al máximo y hacer tu mejor esfuerzo y decir la verdad. Al final, lo que importa es el esfuerzo. El resto está fuera de tu control. - Maria Sharapova, Imparable: mi vida hasta ahora",
+        "description_title": "Perfil personal",
+        "description": "Soy un ingeniero de pila completa que crea aplicaciones modernas",
+        "about_me_title": "Sobre mí",
+        "self_introduction": "Estudiante de grado superior en informática en la Universidad de Columbia Británica. Bueno en autogestión, búsqueda de la perfección, fuerte tolerancia al estrés y entusiasmo más allá de las expectativas.",
+        "soccer": "¡Soy hincha del Real Madrid! ¡Madridista por siempre!",
+        "tech_skills": "Habilidad profesional",
+        "download_resume": "Currículum",
+        "social_media": "Encuéntrame en LinkedIn y GitHub",
+        "project_title": "Proyecto",
+        "bss": "Un pequeño compilador de Lenguaje Específico de Dominio (DSL) que compila BSS en CSS para que permita a los desarrolladores de front-end escribir CSS con reglas de estilo anidadas, variables, funciones, flujo de control, operaciones matemáticas y herencia.",
+        "bloggy": "Una aplicación React de una sola página que permite a los usuarios compartir blogs y permite al personal filtrar artículos por etiquetas, monitorear comentarios y visualizar las estadísticas de los usuarios.",
+        "messageList": "Una aplicación MERN (MongoDB + Express + React + Node.js) que también usa Redux para monitorear estados y desplegar en Heroku. MessageList permite a los usuarios agregar / editar / eliminar mensajes.",
+        "read_more": "Detalles"
     },
     "fr": {
-        "name": "Jean-Jacques Zhou"
+        "name": "Jean-Jacques Zhou",
+        "contact": "détails du contact",
+        "bgc": "détails du contact",
+        "quote": "Vous ne pouvez pas contrôler ce que les gens disent de vous et ce qu’ils pensent de vous. Vous ne pouvez pas prévoir la malchance. Vous ne pouvez que travailler dur, faire de votre mieux et dire la vérité. En fin de compte, c’est l’effort qui compte. Le reste est hors de votre contrôle. - Maria Sharapova, imparable: ma vie jusqu'ici",
+        "description_title": "Profil personnel",
+        "description": "Je suis ingénieur logiciel full stack",
+        "about_me_title": "À propos de moi",
+        "self_introduction": "Un étudiant de premier cycle en informatique à l'Université de la Colombie-Britannique. Bonne gestion de soi, recherche de la perfection, forte tolérance au stress et enthousiasme au-delà des attentes.",
+        "soccer": "Je suis fan du Real Madrid! Madridiste à jamais!",
+        "tech_skills": "Compétence professionnelle",
+        "download_resume": "CV",
+        "social_media": "Retrouvez-moi sur LinkedIn et GitHub",
+        "project_title": "Projets",
+        "bss": "Un petit compilateur DSL (Domain Specific Language) qui compile BSS en CSS afin de permettre aux développeurs frontaux d'écrire du CSS avec des règles de style imbriquées, des variables, des fonctions, un flux de contrôle, des opérations mathématiques et un héritage.",
+        "bloggy": "Une application React à page unique qui permet aux utilisateurs de partager des blogs et permet au personnel de filtrer les articles par balises, de surveiller les commentaires et de visualiser les statistiques des utilisateurs.",
+        "messageList": "Une application MERN (MongoDB + Express + React + Node.js) qui utilise également Redux pour surveiller les états et déployer sur Heroku. MessageList permet aux utilisateurs d'ajouter / modifier / supprimer des messages.",
+        "read_more": "Détails"
     },
     "ae": {
-        "name": "عبد الكريم" //Abd al Karim
+        "name": "عبد الكريم", //Abd al Karim
+        "contact": "بيانات المتصل",
+        "bgc": "تعيين لون المظهر",
+        "quote": "لا يمكنك التحكم في ما يعتقده الناس عنك وما يفكرون فيه. لا يمكنك الاستعداد لسوء الحظ. يمكنك فقط أن تفعل أفضل ما لديك وأن تكون صادقًا أخيرًا ، أهم شيء هو العمل الجاد. الباقي خارج عن إرادتك. - ماريا شارابوفا",
+        "description_title": "الملف الشخصي",
+        "description": "أنا مهندس برمجيات مكدس كامل",
+        "about_me_title": "عني",
+        "self_introduction": "طالب جامعي كبير في الكمبيوتر بجامعة كولومبيا البريطانية. جيد في الإدارة الذاتية ، والسعي لتحقيق الكمال ، وتحمل قوي للتوتر ، وحماس يفوق التوقعات.",
+        "soccer": "أنا من مشجعي ريال مدريد! مدريدي إلى الأبد!",
+        "tech_skills": "مهارة إحترافية",
+        "download_resume": "استئنف",
+        "social_media": "وسائل التواصل الاجتماعي",
+        "project_title": "مشروع مفتوح المصدر",
+        "bss": "مترجم لغة صغير (DSL) يقوم بتجميع BSS إلى CSS بحيث يمكن لمطوري الواجهة الأمامية استخدام قواعد الأنماط المتداخلة والمتغيرات والوظائف وتدفق التحكم والعمليات الرياضية والوراثة لكتابة CSS.",
+        "bloggy": "يتيح تطبيق React أحادي الصفحة للمستخدمين مشاركة المدونات ، ويسمح للخلفية بتصفية المقالات حسب العلامات ، ومراقبة التعليقات ، وتصور إحصاءات المستخدم.",
+        "messageList": "تطبيق MERN (MongoDB + Express + React + Node.js) يستخدم أيضًا Redux لمراقبة الحالة ونشرها في Heroku. يتيح MessageList للمستخدمين إضافة / تحرير / حذف الرسائل.",
+        "read_more": "تفاصيل",
     }
 }
 const langEl = document.getElementsByClassName('language');
-// // const link = 
-// langEl.forEach(el => {
-//     el.addEventListener('click', function(){
-//         console.log(el.getAttribute("id"))
-//     })
-// });
 const title = document.getElementById('name');
 const contact = document.getElementById('contact');
 const bgc = document.getElementById('settings-note');
@@ -101,7 +167,12 @@ const self_intro = document.getElementById('self-introduction');
 const soccer = document.getElementById('soccer');
 const tech_skills = document.getElementById('technical-skills');
 const resume = document.getElementById('download-resume');
-const social_media = document.getElementById('social-media')
+const social_media = document.getElementById('social-media');
+const project_title = document.getElementById('project-title');
+const bss = document.getElementById('bss');
+const bloggy = document.getElementById('bloggy');
+const messageList = document.getElementById('messageList');
+const read_more = document.getElementsByClassName('read-more');
 for (let i=0; langEl.length>i; i++) {
     langEl[i].addEventListener('click', function(){
         console.log(langEl[i].getAttribute("id"));
@@ -117,6 +188,13 @@ for (let i=0; langEl.length>i; i++) {
         tech_skills.textContent = data[langEl[i].getAttribute("id")]["tech_skills"];
         resume.textContent = data[langEl[i].getAttribute("id")]["download_resume"];
         social_media.textContent = data[langEl[i].getAttribute("id")]["social_media"];
+        project_title.textContent = data[langEl[i].getAttribute("id")]["project_title"];
+        bss.textContent = data[langEl[i].getAttribute("id")]["bss"];
+        bloggy.textContent = data[langEl[i].getAttribute("id")]["bloggy"];
+        messageList.textContent = data[langEl[i].getAttribute("id")]["messageList"];
+        read_more[0].textContent = data[langEl[i].getAttribute("id")]["read_more"];
+        read_more[1].textContent = data[langEl[i].getAttribute("id")]["read_more"];
+        read_more[2].textContent = data[langEl[i].getAttribute("id")]["read_more"];
     })
 }
 let theme = localStorage.getItem('theme')
